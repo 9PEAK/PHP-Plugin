8e8c32788b09a7760aaf2bcecd15b3340446c9c4
Kane <52653784@qq.com>
2019-03-06 14:57:29 +0800

    # 增加Encrypt模块，增加凯撒密码组件； # Encrypt模块增加简单字符串处理方法：偏移方法、翻转方法；

---
+++ src/Encrypt/Character.php
@@ -0,0 +1,33 @@
+<?php
+namespace Peak\Plugin\Encrypt;
+# 简单的字符处理
+trait Character
+{
+
+
+    /**
+     * Move the string pointer by the given offset
+     * @param $str. string. the letter or number
+     * @param $x. int. offset for the pointer
+     * @return string. the changed letter
+     * */
+    static function offset($str, $n)
+    {
+        return substr($str, $n).substr($str, 0, $n);
+    }
+
+
+
+    /**
+     * 翻转字符串
+     * @param $str string
+     * */
+    static function flip ($str)
+    {
+        $str = str_split($str);
+        krsort($str);
+        return join('', $str);
+    }
+
+
+}
