5d6a1cc167f1d8055eabfd3b9ea308c7a4b02019
Kane <52653784@qq.com>
2019-05-27 10:29:54 +0800

    FileCache优化：存储文件方法优化，不再强制设置文件权限，调整为存储文件时如果文件不存在则先创建空文件并设置其权限，再存储文件。

--- src/FileCache.php
+++ src/FileCache.php
@@ -49,18 +49,27 @@ class FileCache {
 
 	/**
 	 * 存储文件
+	 * @param $dat mixed 要存储的数据，string,int,object,array，其中object和array类型的数据将最终转化成json存储。
+	 * @param $mode bool 是否设置文件权限
 	 * @return bool
 	 * */
-	protected function save_content ($dat)
+	protected function save_content ($dat, $mode=false)
 	{
+
+		if (!file_exists($this->file)) {
+			// 尝试创建文件
+			if (!$this->save_content('', true)) {
+				return false;
+			}
+		}
 
 		$res = file_put_contents($this->file, is_string($dat) ? $dat : json_encode($dat));
 		if ($res===false) {
 			return self::debug('无法写入文件，路径：“'.$this->file.'”。');
 		}
 
-		if ($this->rwx) {
-			if (!chmod($this->file, $this->rwx)) {
+		if ($mode) {
+			if (@!chmod($this->file, $this->rwx)) {
 				return self::debug('无法设置文件权限，路径：“'.$this->file.'”。');
 			}
 		}
