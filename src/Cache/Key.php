08e4e5f378bce5835d8c860ad2f2b916a79778c7
Kane <52653784@qq.com>
2019-07-01 09:19:49 +0800

    修复PropertyTranslater批量翻译时，指定属性名报错的bug；恢复SQL组件。

--- src/Cache/Key.php
+++ src/Cache/Key.php
@@ -10,5 +10,4 @@ trait Key
 		return static::class.':'.$id;
 	}
 
-
 }
--- src/PropertyTranslater/Core.php
+++ src/PropertyTranslater/Core.php
@@ -81,19 +81,20 @@ trait Core
 	/**
 	 * 基于$translation配置，转化翻译指定的或所有的属性
 	 * @param $prop mixed string|array|null 指定需要翻译的属性，如果是string，多个属性用','分割；如果为空，则翻译所有属性
-	 * @param $prf 返回属性名的前缀
-	 * @param $ext 返回属性名的后缀
-	 * @return 成功true；如果Model未指定translate属性名，则返回false。
+	 * @param $prf string 返回属性名的前缀
+	 * @param $ext string 返回属性名的后缀
+	 * @return bool 成功true；如果Model未指定translate属性名，则返回false。
 	 * */
 	public function translateProperties ($prop=null, $prf='_', $ext=''):bool
 	{
 
 		if ( $prop ) {
 			$prop = is_array($prop) ? $prop : explode(',', $prop);
-			$prop = array_intersect_assoc(array_flip($prop), static::translation());
+			$prop = array_intersect_key(array_flip($prop), static::translation());
 		} else {
-			$prop = array_keys(static::translation());
+			$prop = static::translation();
 		}
+		$prop = array_keys($prop);
 
 		foreach ( $prop as $k ) {
 			$this->{$prf.$k.$ext} = $this->translateProperty($k);
