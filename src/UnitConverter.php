3799a39f9d86653a63b50ec81bf2434c2f4b2abe
Kane <52653784@qq.com>
2020-10-22 16:32:59 +0800

    # “单位转换计算器”增加全局函数，修改成Trait模式

--- src/UnitConverter.php
+++ src/UnitConverter.php
@@ -4,10 +4,7 @@
  * @author Kane
  */
 
-namespace Peak\Plugin;
-
-class UnitConverter
-{
+namespace Peak\UnitConverter {
 
     const UNIT = [
         # 重量单位
@@ -32,28 +29,51 @@ class UnitConverter
     ];
 
 
+    trait Core
+    {
+        /**
+         * 转化计量单位
+         * @param float $val
+         * @param string $from 初始单位
+         * @param string $to 转化单位
+         * @param int $prec 精度，默认2精确到小数点后两位
+         * @return float|null 如遇到无法计算的单位返回false
+         */
+        static function convertMeasurement ($val, $from, $to, $prec=2)
+        {
+            // 保证单位是支持的
+            $from = strtolower($from);
+            $to = strtolower($to);
+
+            if (@UNIT[$from] && @UNIT[$to]) {
+                if ($rate = @RATE[$from . '/' . $to]) {
+                    return round($val / $rate, $prec);
+                } else if ($rate = @RATE[$to . '/' . $from]) {
+                    return round($rate * $val, $prec);
+                }
+            }
+
+            return false;
+        }
+    }
+
+
+}
+
+
+namespace {
+
     /**
-     * 计算
+     * 转化计量单位
      * @param float $val
      * @param string $from 初始单位
      * @param string $to 转化单位
      * @param int $prec 精度，默认2精确到小数点后两位
      * @return float|false 如遇到无法计算的单位返回false
      */
-    static function translate ($val, $from, $to, $prec=2)
+    function convert_measurement ($val, $from, $to, $prec=2)
     {
-        $from = strtolower($from);
-        $to = strtolower($to);
-
-        // 保证单位是支持的
-        if (@self::UNIT[$from] && @self::UNIT[$to]) {
-            if ($rate = @self::RATE[$from . '/' . $to]) {
-                return round($val / $rate, $prec);
-            } else if ($rate = @self::RATE[$to . '/' . $from]) {
-                return round($rate * $val, $prec);
-            }
-        }
-        return false;
+        return \Peak\UnitConverter\Core::convertMeasurement($val, $from, $to, $prec);
     }
 
 }
